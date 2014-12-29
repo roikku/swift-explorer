61d20b2ee3caab8e532e468dea91dddcb9f255be
Loic Merckel <loic.merckel@gmail.com>
2014-12-29 11:51:53 +0100

    version update for release 1.0.7

--- src/main/java/org/swiftexplorer/config/Configuration.java
+++ src/main/java/org/swiftexplorer/config/Configuration.java
@@ -46,7 +46,7 @@ public enum Configuration implements HasConfiguration {
 	private final String defaultXmlPath ; 
 	
 	private final String appName = "Swift Explorer" ;
-	private final String appVersion = "1.0.7-SNAPSHOT" ;
+	private final String appVersion = "1.0.7" ;
 	
 	@Override
 	public String getAppName ()
